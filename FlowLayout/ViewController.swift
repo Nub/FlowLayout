9c224febda48e9129d1aea3b321b797f1fb085bd
Zachry Thayer <zachthayer@gmail.com>
2014-07-16 10:05:05 -0700

    Changed animation

--- FlowLayout/ViewController.swift
+++ FlowLayout/ViewController.swift
@@ -11,7 +11,7 @@ import QuartzCore
 
 class ViewController: UIViewController {
 	
-	var layoutView = View(frame: CGRect(x: 150, y: 150, width: 50, height: 50))
+	var layoutView = View(frame: CGRectZero)
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -24,6 +24,7 @@ class ViewController: UIViewController {
 	
 	func style() {
 		layoutView.backgroundColor = UIColor.blackColor()
+		layoutView.frame = self.view.bounds
 //		layoutView.centerContents = true
 	}
 	
@@ -32,33 +33,19 @@ class ViewController: UIViewController {
 		let a = View(frame: CGRect(x: 0, y: 0, width: 170, height: 20))
 		a.backgroundColor = UIColor.yellowColor()
 		
-		let a1 = View(frame: CGRect(x: 0, y: 0, width: 20, height: 50))
-		a1.backgroundColor = UIColor.yellowColor()
-
-		let b = View(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
-		b.backgroundColor = UIColor.blueColor()
-//		b.margin.right = 10
-		
-		let c = View(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
-		c.backgroundColor = UIColor.greenColor()
-//		c.margin.left = 20
-		
 		self.layoutView.addSubview(a)
-		self.layoutView.addSubview(a1)
-
-		self.layoutView.addSubview(b)
-		self.layoutView.addSubview(c)
 		
 		for i in 0..<50 {
 			let a = View(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
 			a.backgroundColor = UIColor.blueColor()
 			a.margin.bottom = 1
+			a.margin.right = 1
 			self.layoutView.addSubview(a)
 		}
 		
 		dispatch_async(dispatch_get_main_queue()){
-			UIView.animateWithDuration(5.0, delay: 0, options: UIViewAnimationOptions.LayoutSubviews, animations: {
-				self.layoutView.frame.inset(dx: -100, dy: -100)
+			UIView.animateWithDuration(5.0, delay: 1.0, options: UIViewAnimationOptions.LayoutSubviews, animations: {
+				self.layoutView.frame = CGRect(x: 0, y: 0, width: 200, height: self.view.bounds.size.height)
 				}, completion: nil)
 		}
 	}
